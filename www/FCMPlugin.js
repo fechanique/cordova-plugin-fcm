a5f1aeddb88674d36d89ebf340d5edfd4c4049fb
Frank Rue <frankrue@gmail.com>
2016-12-09 11:37:35 -0500

    fixed javascript execs

--- www/FCMPlugin.js
+++ www/FCMPlugin.js
@@ -1,43 +1,50 @@
-cordova.define("cordova-plugin-fcm.FCMPlugin", function(require, exports, module) {
 var exec = require('cordova/exec');
 
-exports.subscribeToTopic = function(topic,success, error) {
-    exec(success, error, "FCMPlugin", "subscribeToTopic", [topic]);
-};
-
-exports.getToken = function(success, error) {
-    exec(success, error, "FCMPlugin", "getToken", []);
-};
-
-exports.setBadgeNumber = function(number, success, error) {
-    exec(success, error, "FCMPlugin", "setBadgeNumber", [number]);
-};
-
-exports.getBadgeNumber = function(success, error) {
-    exec(success, error, "FCMPlugin", "getBadgeNumber", []);
-};
-
-
-exports.unsubscribeFromTopic = function( topic, success, error ){
-	exec(success, error, "FCMPlugin", 'unsubscribeFromTopic', [topic]);
+function FCMPlugin() {
+  console.log("FCMPlugin.js: is created");
 }
 
-
-// NOTIFICATION CALLBACK //
-exports.onNotification = function( callback, success, error ){
-	FCMPlugin.onNotificationReceived = callback;
-	exec(success, error, "FCMPlugin", 'registerNotification',[]);
-}
-// DEFAULT NOTIFICATION CALLBACK //
-exports.onNotificationReceived = function(payload){
-	console.log("Received push notification")
-	console.log(payload)
-}
 
 // SET BADGE NUMBER //
-exports.setBadgeNumber = function(number, success, error){
-	exec(success, error, "FCMPlugin", 'setBadgeNumber',[number]);
-	
+FCMPlugin.prototype.setBadgeNumber = function(number, success, error) {
+  exec(success, error, "FCMPlugin", "setBadgeNumber", [number]);
+};
+
+// GET BADGE NUMBER //
+FCMPlugin.prototype.getBadgeNumber = function(success, error) {
+  exec(success, error, "FCMPlugin", "getBadgeNumber", []);
+};
+
+// GET TOKEN //
+FCMPlugin.prototype.getToken = function( success, error ){
+  exec(success, error, "FCMPlugin", 'getToken', []);
 }
 
-});
+// SUBSCRIBE TO TOPIC //
+FCMPlugin.prototype.subscribeToTopic = function( topic, success, error ){
+  exec(success, error, "FCMPlugin", 'subscribeToTopic', [topic]);
+}
+
+// UNSUBSCRIBE FROM TOPIC //
+FCMPlugin.prototype.unsubscribeFromTopic = function( topic, success, error ){
+  exec(success, error, "FCMPlugin", 'unsubscribeFromTopic', [topic]);
+}
+
+// NOTIFICATION CALLBACK //
+FCMPlugin.prototype.onNotification = function( callback, success, error ){
+  FCMPlugin.prototype.onNotificationReceived = callback;
+  exec(success, error, "FCMPlugin", 'registerNotification',[]);
+}
+
+// DEFAULT NOTIFICATION CALLBACK //
+FCMPlugin.prototype.onNotificationReceived = function(payload){
+  console.log("Received push notification")
+  console.log(payload)
+}
+
+// FIRE READY //
+exec(function(result){ console.log("FCMPlugin Ready OK") }, function(result){ console.log("FCMPlugin Ready ERROR") }, "FCMPlugin",'ready',[]);
+
+
+var fcmPlugin = new FCMPlugin();
+module.exports = fcmPlugin;
