3b57bfb3ea4e03452222e9317eae804023c316ed
Christopher Palmer <chrisjpalmer@optusnet.com.au>
2017-09-06 11:13:00 +1000

    Fixed compiler warning about duplicate category method

--- src/ios/AppDelegate+FCMPlugin.h
+++ src/ios/AppDelegate+FCMPlugin.h
@@ -10,6 +10,19 @@
 #import <UIKit/UIKit.h>
 #import <Cordova/CDVViewController.h>
 
-@interface AppDelegate (FCMPlugin)
+#import "Firebase.h"
+
+// Copied from Apple's header in case it is missing in some cases (e.g. pre-Xcode 8 builds).
+#ifndef NSFoundationVersionNumber_iOS_9_x_Max
+#define NSFoundationVersionNumber_iOS_9_x_Max 1299
+#endif
+
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+@import UserNotifications;
+
+@interface AppDelegate (FCMPlugin) <UNUserNotificationCenterDelegate, FIRMessagingDelegate>
 
 @end
+#endif
+
+
--- src/ios/AppDelegate+FCMPlugin.m
+++ src/ios/AppDelegate+FCMPlugin.m
@@ -10,27 +10,11 @@
 #import <objc/runtime.h>
 #import <Foundation/Foundation.h>
 
-#import "Firebase.h"
-
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-@import UserNotifications;
-#endif
-
 // Implement UNUserNotificationCenterDelegate to receive display notification via APNS for devices
 // running iOS 10 and above. Implement FIRMessagingDelegate to receive data message via FCM for
 // devices running iOS 10 and above.
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-@interface AppDelegate () <UNUserNotificationCenterDelegate, FIRMessagingDelegate>
-
-@end
-#endif
 
 static NSData* lastNotification;
-
-// Copied from Apple's header in case it is missing in some cases (e.g. pre-Xcode 8 builds).
-#ifndef NSFoundationVersionNumber_iOS_9_x_Max
-#define NSFoundationVersionNumber_iOS_9_x_Max 1299
-#endif
 
 @implementation AppDelegate (FCMPlugin)
 
