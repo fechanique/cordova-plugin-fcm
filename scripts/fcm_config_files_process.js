95a167e27173cc7c201b5927368caa2cbbaa3df1
Stanislav Sidoryuk <staskuban@ya.ru>
2018-07-11 19:30:53 +0300

    fix

--- scripts/fcm_config_files_process.js
+++ scripts/fcm_config_files_process.js
@@ -19,7 +19,7 @@ fs.ensureDirSync = function (dir) {
 var resolvePath = function (dir) {
     if (fs.existsSync(dir + "/res/values"))
         return "/res/values/";
-    else return "app/src/main/res/values/"
+    else return "/app/src/main/res/values/"
 }
 
 
