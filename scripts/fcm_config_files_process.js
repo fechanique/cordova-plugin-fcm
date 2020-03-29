b73a1bcda64f13686d9f6d268bcff3bb4d036687
André Tissot <andrehtissot@gmail.com>
2020-03-29 19:54:57 +0200

    install nodejs script refactored
    Now, less exceptions will be silent, bringing more feedback to the user.
    With colored output and bold letters for both warnings and errors.
    
    The string.xml is now also supported if found in `platforms/android/app/src/main/res/values/strings.xml` too.

--- scripts/fcm_config_files_process.js
+++ scripts/fcm_config_files_process.js
@@ -2,120 +2,145 @@
 'use strict';
 
 var fs = require('fs');
-var path = require('path');
-
-fs.ensureDirSync = function(dir) {
-    if (!fs.existsSync(dir)) {
-        dir.split(path.sep).reduce(function(currentPath, folder) {
-            currentPath += folder + path.sep;
-            if (!fs.existsSync(currentPath)) {
-                fs.mkdirSync(currentPath);
-            }
-            return currentPath;
-        }, '');
-    }
-};
 
 var config = fs.readFileSync('config.xml').toString();
 var name = getValue(config, 'name');
 
 var IOS_DIR = 'platforms/ios';
 var ANDROID_DIR = 'platforms/android';
-var ANDROID_DIR_RES_VALUES_DIR = ANDROID_DIR + '/res/values';
 
 var PLATFORM = {
     IOS: {
-        dest: [
+        googleServiceDestinations: [
             IOS_DIR + '/' + name + '/Resources/GoogleService-Info.plist',
             IOS_DIR + '/' + name + '/Resources/Resources/GoogleService-Info.plist'
         ],
-        src: [
+        googleServiceSources: [
             'GoogleService-Info.plist',
             IOS_DIR + '/www/GoogleService-Info.plist',
             'www/GoogleService-Info.plist'
         ]
     },
     ANDROID: {
-        dest: [ANDROID_DIR + '/google-services.json', ANDROID_DIR + '/app/google-services.json'],
-        src: [
+        googleServiceDestinations: [
+            ANDROID_DIR + '/google-services.json',
+            ANDROID_DIR + '/app/google-services.json'
+        ],
+        googleServiceSources: [
             ANDROID_DIR + '/google-services.json',
             ANDROID_DIR + '/assets/www/google-services.json',
             'www/google-services.json',
             'google-services.json'
         ],
-        stringsXml: ANDROID_DIR_RES_VALUES_DIR + '/strings.xml'
+        stringsXmls: [
+            ANDROID_DIR + '/res/values/strings.xml',
+            ANDROID_DIR + '/app/src/main/res/values/strings.xml'
+        ]
     }
 };
 
-// Copy key files to their platform specific folders
 if (directoryExists(IOS_DIR)) {
-    copyKey(PLATFORM.IOS);
+    copyGoogleServiceFile(PLATFORM.IOS);
 }
 if (directoryExists(ANDROID_DIR)) {
-    copyKey(PLATFORM.ANDROID, updateStringsXml);
+    copyGoogleServiceFile(PLATFORM.ANDROID, updateAndroidStringsXml);
 }
 
-function updateStringsXml(contents) {
-    if (!directoryExists(ANDROID_DIR_RES_VALUES_DIR)) {
+function updateAndroidStringsXml(contents) {
+    var stringXmlPath = findExistingFilePath(PLATFORM.ANDROID.stringsXmls);
+    if (!stringXmlPath) {
+        logWarning('Android-specific strings.xml file not found!');
         return;
     }
 
-    var json = JSON.parse(contents);
-    var strings = fs.readFileSync(PLATFORM.ANDROID.stringsXml).toString();
+    // Current string values
+    var strings = fs.readFileSync(stringXmlPath).toString();
 
-    // strip non-default value
-    strings = strings.replace(
-        new RegExp('<string name="google_app_id">([^@<]+?)</string>', 'i'),
-        ''
-    );
+    // Read Google Service app id and key
+    var googleServiceClient;
+    try {
+        googleServiceClient = JSON.parse(contents).client[0];
+    } catch (error) {
+        logError('google-services.json found was not a valid JSON', error);
+    }
 
-    // strip non-default value
-    strings = strings.replace(
-        new RegExp('<string name="google_api_key">([^@<]+?)</string>', 'i'),
-        ''
-    );
+    var entries = [
+        {
+            name: 'google_app_id',
+            value: googleServiceClient.client_info.mobilesdk_app_id
+        },
+        {
+            name: 'google_api_key',
+            value: googleServiceClient.api_key[0].current_key
+        }
+    ];
+
+    entries.forEach(function (entry) {
+        // If found, remove old entry
+        strings = strings.replace(
+            new RegExp('<string name="' + entry.name + '">([^<]+?)</string>', 'i'),
+            '\n'
+        );
+
+        var newEntryStartPosition = strings.indexOf('</resources>');
+        if (newEntryStartPosition === -1) {
+            logError('string.xml unrecognizable!', strings);
+            return;
+        }
+
+        strings =
+            strings.substr(0, newEntryStartPosition) +
+            '    <string name="' +
+            entry.name +
+            '">' +
+            entry.value +
+            '</string>\n' +
+            strings.substr(newEntryStartPosition);
+    });
 
     // strip empty lines
-    strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', 'gm'), '$1');
+    strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', 'gm'), '');
 
-    // replace the default value
-    strings = strings.replace(
-        new RegExp('<string name="google_app_id">([^<]+?)</string>', 'i'),
-        '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>'
-    );
-
-    // replace the default value
-    strings = strings.replace(
-        new RegExp('<string name="google_api_key">([^<]+?)</string>', 'i'),
-        '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>'
-    );
-
-    fs.writeFileSync(PLATFORM.ANDROID.stringsXml, strings);
+    fs.writeFileSync(stringXmlPath, strings);
 }
 
-function copyKey(platform, callback) {
-    for (var i = 0; i < platform.src.length; i++) {
-        var file = platform.src[i];
-        if (fileExists(file)) {
-            try {
-                var contents = fs.readFileSync(file).toString();
+function copyGoogleServiceFile(platform, callback) {
+    var googleServiceSourcePath = findExistingFilePath(platform.googleServiceSources);
+    if (!googleServiceSourcePath) {
+        if (platform === PLATFORM.ANDROID) {
+            logWarning('Android-specific google-services.json file not found!');
+        } else {
+            logWarning('iOS-specific GoogleService-Info.plist file not found!');
+        }
+        return;
+    }
 
-                try {
-                    platform.dest.forEach(function(destinationPath) {
-                        var folder = destinationPath.substring(0, destinationPath.lastIndexOf('/'));
-                        fs.ensureDirSync(folder);
-                        fs.writeFileSync(destinationPath, contents);
-                    });
-                } catch (e) {
-                    // skip
-                }
+    var contents;
+    try {
+        contents = fs.readFileSync(googleServiceSourcePath).toString();
+    } catch (error) {
+        logError('Error on trying to read ' + googleServiceSourcePath, error);
+        return;
+    }
+    platform.googleServiceDestinations.forEach(function (destinationPath) {
+        try {
+            ensureFileDirExistance(destinationPath);
+            fs.writeFileSync(destinationPath, contents);
+        } catch (error) {
+            logError('Error on trying to write ' + destinationPath, error);
+            return;
+        }
+    });
 
-                callback && callback(contents);
-            } catch (err) {
-                console.log(err);
-            }
+    if (callback) {
+        callback(contents);
+    }
+}
 
-            break;
+function findExistingFilePath(paths) {
+    for (var i = paths.length - 1; i > -1; i--) {
+        if (fileExists(paths[i])) {
+            return paths[i];
         }
     }
 }
@@ -143,4 +168,33 @@ function directoryExists(path) {
     } catch (e) {
         return false;
     }
+}
+
+function ensureFileDirExistance(filePath) {
+    var dirPath = filePath.substring(0, filePath.lastIndexOf('/'));
+    ensureDirExistance(dirPath);
+}
+
+function ensureDirExistance(dirPath) {
+    if (!fs.existsSync(dirPath)) {
+        dirPath.split('/').reduce(function (currentPath, folder) {
+            currentPath += folder + '/';
+            if (!fs.existsSync(currentPath)) {
+                fs.mkdirSync(currentPath);
+            }
+            return currentPath;
+        }, '');
+    }
+}
+
+function logError(message, obj) {
+    if (obj) {
+        console.error('\x1b[1m\x1b[31m%s\n%O\x1b[0m', message, obj);
+    } else {
+        console.error('\x1b[1m\x1b[31m%s\x1b[0m', message);
+    }
+}
+
+function logWarning(message) {
+    console.warn('\x1b[1m\x1b[33m%s\x1b[0m', message);
 }
