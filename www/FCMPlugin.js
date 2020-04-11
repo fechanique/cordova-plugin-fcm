d403e8b3a40299f09022b09c232261ffcd01652b
André Tissot <andrehtissot@gmail.com>
2020-04-11 22:46:20 +0200

    Removed, soon-to-be unsupported, onFirebaseDataNotificationIOS method

--- www/FCMPlugin.js
+++ www/FCMPlugin.js
@@ -6,7 +6,7 @@ function FCMPlugin() {
 }
 
 // CHECK FOR PERMISSION
-FCMPlugin.prototype.hasPermission = function(success, error) {
+FCMPlugin.prototype.hasPermission = function (success, error) {
   if (cordova.platformId !== "ios") {
     success(true);
     return;
@@ -15,38 +15,33 @@ FCMPlugin.prototype.hasPermission = function(success, error) {
 };
 
 // SUBSCRIBE TO TOPIC //
-FCMPlugin.prototype.subscribeToTopic = function(topic, success, error) {
+FCMPlugin.prototype.subscribeToTopic = function (topic, success, error) {
   exec(success, error, "FCMPlugin", "subscribeToTopic", [topic]);
 };
 
 // UNSUBSCRIBE FROM TOPIC //
-FCMPlugin.prototype.unsubscribeFromTopic = function(topic, success, error) {
+FCMPlugin.prototype.unsubscribeFromTopic = function (topic, success, error) {
   exec(success, error, "FCMPlugin", "unsubscribeFromTopic", [topic]);
 };
 
 // NOTIFICATION CALLBACK //
-FCMPlugin.prototype.onNotification = function(callback, success, error) {
+FCMPlugin.prototype.onNotification = function (callback, success, error) {
   FCMPlugin.prototype.onNotificationReceived = callback;
   exec(success, error, "FCMPlugin", "registerNotification", []);
 };
 
-// FIREBASE DATA NOTIFICATION CALLBACK //
-FCMPlugin.prototype.onFirebaseDataNotificationIOS = function (callback) {
-    FCMPlugin.prototype.onFirebaseDataNotificationReceivedIOS = callback;
-};
-
 // TOKEN REFRESH CALLBACK //
-FCMPlugin.prototype.onTokenRefresh = function(callback) {
+FCMPlugin.prototype.onTokenRefresh = function (callback) {
   FCMPlugin.prototype.onTokenRefreshReceived = callback;
 };
 
 // GET TOKEN //
-FCMPlugin.prototype.getToken = function(success, error) {
+FCMPlugin.prototype.getToken = function (success, error) {
   exec(success, error, "FCMPlugin", "getToken", []);
 };
 
 // GET APNS TOKEN //
-FCMPlugin.prototype.getAPNSToken = function(success, error) {
+FCMPlugin.prototype.getAPNSToken = function (success, error) {
   if (cordova.platformId !== "ios") {
     success(null);
     return;
@@ -55,34 +50,28 @@ FCMPlugin.prototype.getAPNSToken = function(success, error) {
 };
 
 // CLEAR ALL NOTIFICATIONS //
-FCMPlugin.prototype.clearAllNotifications = function(success, error) {
+FCMPlugin.prototype.clearAllNotifications = function (success, error) {
   exec(success, error, "FCMPlugin", "clearAllNotifications", []);
 };
 
 // DEFAULT NOTIFICATION CALLBACK //
-FCMPlugin.prototype.onNotificationReceived = function(payload) {
+FCMPlugin.prototype.onNotificationReceived = function (payload) {
   console.log("Received push notification");
   console.log(payload);
 };
 
-// DEFAULT FIREBASE DATA NOTIFICATION CALLBACK //
-FCMPlugin.prototype.onFirebaseDataNotificationReceivedIOS = function (payload) {
-    console.log('Received Firebase data notification');
-    console.log(payload);
-};
-
 // DEFAULT TOKEN REFRESH CALLBACK //
-FCMPlugin.prototype.onTokenRefreshReceived = function(token) {
+FCMPlugin.prototype.onTokenRefreshReceived = function (token) {
   console.log("Received token refresh");
   console.log(token);
 };
 
 // FIRE READY //
 exec(
-  function(result) {
+  function (result) {
     console.log("FCMPlugin Ready OK");
   },
-  function(result) {
+  function (result) {
     console.log("FCMPlugin Ready ERROR");
   },
   "FCMPlugin",
