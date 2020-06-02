2aa072dd7390ec57192c1a1e823fe5edadc3df55
André Tissot <andrehtissot@gmail.com>
2020-06-02 20:44:06 +0200

    Ionic definition inspired on current version and ngx

--- ionic/ngx/FCMPluginOnIonicNGX.d.ts
+++ ionic/ngx/FCMPluginOnIonicNGX.d.ts
@@ -1,3 +1,15 @@
 import { FCMPluginOnIonic } from "../FCMPluginOnIonic";
-export declare class FCMPluginOnIonicNGX extends FCMPluginOnIonic {
+declare class FCMPluginOnIonicNGX extends FCMPluginOnIonic {
+    static pluginName: string;
+    static plugin: string;
+    static pluginRef: string;
+    static repo: string;
+    static platforms: string[];
+    static installed: () => boolean;
+    static getPlugin: () => any;
+    static getPluginName: () => string;
+    static getPluginRef: () => string;
+    static getPluginInstallName: () => string;
+    static getSupportedPlatforms: () => string[];
 }
+export { FCMPluginOnIonicNGX };
