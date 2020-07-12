2c2c94132023f4c68030b9443d8ae70263a773ac
André Tissot <andrehtissot@gmail.com>
2020-07-12 21:04:05 +0200

    Improved install_ionic_dependencies compatibility with windows

--- scripts/install_ionic_dependencies.sh
+++ scripts/install_ionic_dependencies.sh
@@ -1,6 +1,9 @@
-cd node_modules/cordova-plugin-fcm-with-dependecy-updated/ionic/
-npm i
+cd node_modules
+cd cordova-plugin-fcm-with-dependecy-updated
+cd ionic
+npm i --loglevel error --no-progress
 cd ngx
-npm i
-cd ../v4
-npm i
+npm i --loglevel error --no-progress
+cd ..
+cd v4
+npm i --loglevel error --no-progress
