b516c35c43f6b62da5465c6673c669f55a734079
André Tissot <andrehtissot@gmail.com>
2020-11-08 19:45:17 +0100

    Testing omission of ionic dep install for pure cordova projects

--- scripts/install_ionic_dependencies.bat
+++ scripts/install_ionic_dependencies.bat
@@ -1,9 +1,6 @@
 cd node_modules
 cd cordova-plugin-fcm-with-dependecy-updated
-cd ionic
-npm i --loglevel error --no-progress
-cd ngx
-npm i --loglevel error --no-progress
-cd ..
-cd v4
-npm i --loglevel error --no-progress
+cd scripts
+node install_ionic_dependencies.js ionic
+#node install_ionic_dependencies.js ionic/ngx
+#node install_ionic_dependencies.js ionic/v4
---
+++ scripts/install_ionic_dependencies.js
@@ -0,0 +1,47 @@
+const helpers = require('./helpers');
+
+const shouldInstallIonicDependencies = function () {
+    const fs = require('fs');
+    const packageFilePath = `${process.cwd()}/../../../package.json`;
+    if (!helpers.fileExists(packageFilePath)) {
+        helpers.logWarning('package.json was not found.');
+        helpers.logWarning('Ionic dependencies omission cannot be safelly skipped.');
+        return true;
+    }
+    let packageDataString;
+    try {
+        packageDataString = fs.readFileSync(packageFilePath);
+    } catch (e) {
+        helpers.logWarning('package.json found is unreadable.', e);
+        helpers.logWarning('Ionic dependencies omission cannot be safelly skipped.');
+        return true;
+    }
+    let packageData;
+    try {
+        packageData = JSON.parse(packageDataString);
+    } catch (e) {
+        helpers.logWarning('package.json could not be parsed.', e);
+        helpers.logWarning('Ionic dependencies omission cannot be safelly skipped.');
+        return true;
+    }
+    return !!(
+        packageData &&
+        packageData.dependencies &&
+        packageData.dependencies['@ionic-native/core']
+    );
+};
+
+const installIonicDependencies = function () {
+    helpers.execute('npm', ['install', '--loglevel', 'error', '--no-progress']).catch(function (e) {
+        helpers.logError('Failed to auto install Ionic dependencies!', e);
+        helpers.logError(
+            `Please run \`cd node_modules/cordova-plugin-fcm-with-dependecy-updated/${process.argv[2]}; npm install\` manually`
+        );
+    });
+};
+
+if (shouldInstallIonicDependencies()) {
+    installIonicDependencies();
+} else {
+    console.log('Ionic dependencies install skipped :)');
+}
